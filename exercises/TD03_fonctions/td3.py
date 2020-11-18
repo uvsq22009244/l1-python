e0709ec7f04345689fa267423dba2fdc3964eb9b
uvsq22009244 <princesse.momo@ens.uvsq.fr>
2020-11-18 18:50:27 +0100

    correction td3

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -21,8 +21,54 @@ temps = secondeEnTemps(100000)
 print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
 
 
+def affichePluriel(val,mot):
+    if val != 0:
+      print("",val,mot,end = "")
+    if val > 1:
+      print("s",end = "")
+
+
 def afficheTemps(temps):
-    return(jour, heure, minute, seconde)
+    affichePluriel(temps[0],"jour")
+    affichePluriel(temps[1],"heure")
+    affichePluriel(temps[2],"minute")
+    affichePluriel(temps[3],"seconde")
+    print("")
     
-afficheTemps((1,0,14,23))    
-print(afficheTemps[0],"jours", afficheTemps[1], "heures", afficheTemps[2], "minutes", afficheTemps[3], "secondes")
+afficheTemps((1,0,14,23))
+
+
+
+def demandeTemps():
+    jours = int(input("Entrer un nombre de jours"))
+    heures = int(input("Entrer une heureS1"))
+    minutes = int(input("Entrer des minutes"))
+    secondes = int(input("Entrer des secondes"))
+    
+    return(jours, heures, minutes, secondes)
+afficheTemps(demandeTemps())    
+
+
+def sommeTemps(temps1,temps2):
+    return secondeEnTemps(tempsEnSeconde(temps1) + tempsEnSeconde(temps2))
+
+sommeTemps((2,3,4,25),(5,22,57,1))
+
+import time
+def tempsEnDate(temps):
+    jour, heure, minute, seconde = temps
+    annee = 1970 + jour // 365
+    jour %= 365 
+    return (annee, jour, heure, minute, seconde)
+
+def afficheDate(date = -1):
+    if date == -1:
+      date = tempsEnDate(secondeEnTemps(int(time.time())))
+    annee, jour, heure, minute, seconde = date
+    print("Année", annee, end = " ")
+    afficheTemps((jour%365, heure, minute, seconde))
+    
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDate(temps))
+afficheDate()
